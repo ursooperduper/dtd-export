7d397f9fd489cf8d0e14eb9c1393460bc76c2a5b
Sarah Kuehnle <sarah@sooperduper.com>
2015-10-01 18:03:27 -0400

    Updates README and adds the Javascript Adobe Photoshop script.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # dtd-export
-Opens "Don't think... draw" PSD asset files and saves each layer as a GIF or JPG depending on how the layer is named. 
+
+This Adobe Photoshop script exports layers from Don't think... draw
+PSDs, saving them as web-ready GIFs or JPGs.
+
+There are two sets of PSDs it processes - thumbnails and full-size images.
+The originating PSDs have prepared artwork in the desired dimensions.
+
+Other items to note:
+* Layers with just number labels are exported as GIFs.
+* Layers with a number + "-color" are exported as JPGS.
+* Thumbnail images have a light grey background.
+* Fullsize images export with a white background.
---
+++ dtd-export.js
@@ -0,0 +1,113 @@
+/*
+  Filename:   dtd-export.js
+  Title:      "Don't think... draw" export script
+  Author:     Sarah Kuehnle
+  Created:    October 1, 2015
+*/
+
+// First, close all open documents
+while (app.documents.length) {
+  app.activeDocument.close();
+}
+
+// Initiatlize variables
+var numFiles = 4;
+var thumbsArray = [];
+var fullSizeArray = [];
+var thumbsFileName;
+var fullSizeFileName;
+var exportOptions;
+var fileExt;
+var layerName;
+var docRef;
+var layerCount;
+var fileToOpen;
+var matteColor;
+
+// Populate arrays with the appropriate filenames
+for (var i = 1; i <= numFiles; i++) {
+  thumbsFileName   = "dtd-thumbs-" + i + ".psd";
+  fullSizeFileName = "dtd-full-size-" + i + ".psd";
+  thumbsArray.push(thumbsFileName);
+  fullSizeArray.push(fullSizeFileName);
+}
+
+// The root path where assets are located
+var docRootPath = "/Users/sarah/Dropbox/DTD-2014/dtd-assets-working/";
+
+// GIF export options
+var exportGIFOptions = new ExportOptionsSaveForWeb();
+exportGIFOptions.colors       = 32;
+exportGIFOptions.dither       = Dither.NONE;
+exportGIFOptions.transparency = false;
+exportGIFOptions.format       = SaveDocumentType.COMPUSERVEGIF;
+
+// JPG export options
+var exportJPGOptions = new ExportOptionsSaveForWeb();
+exportJPGOptions.format       = SaveDocumentType.JPEG;
+exportJPGOptions.optimize     = true;
+exportJPGOptions.quality      = 80;
+
+// Loops through files and export
+for (var i = 0; i < numFiles; i++) {
+  exportToWeb("full-size", "GIF", i);
+  exportToWeb("thumbs", "GIF", i);
+}
+
+// Function: exportToWeb
+// Parameters:
+//  - type: full-size or thumbs
+//  - format: GIF or JPG
+//  -  num: file number
+function exportToWeb(type, format, num) {
+  matteColor = new RGBColor();
+
+  // Determine which array and background color to use
+  if (type == 'full-size') {
+    artArray = fullSizeArray;
+    matteColor.red   = 255;
+    matteColor.green = 255;
+    matteColor.blue  = 255;
+  } else {
+    artArray = thumbsArray;
+    matteColor.red   = 148;
+    matteColor.green = 148;
+    matteColor.blue  = 148;
+  }
+
+  // Only GIFs need a background set. Color items have styled backgrounds
+  // as a part of the art work.
+  exportGIFOptions.matteColor = matteColor;
+
+  // Open the file
+  fileToOpen = open(File(docRootPath + type + "/" + artArray[num]));
+
+  // Set the file as active and count its layers
+  docRef = app.activeDocument;
+  layerCount = docRef.layers.length;
+
+  // Loop through each layer and export image
+  for (var i = 0; i < layerCount; i++) {
+    layerName = docRef.layers[i].name;
+
+    // Determine wether a GIF or JPG should be output:
+    // Look for layers with "-color" in the name, these should be JPGS.
+    if (layerName.indexOf("-color") == -1) {
+      exportOptions = exportGIFOptions;
+      fileExt = ".gif";
+    } else {
+      exportOptions = exportJPGOptions;
+      fileExt = ".jpg";
+    }
+
+    // Show the layer
+    docRef.layers[i].visible = true;
+    // Export the layer
+    docRef.exportDocument(new File(docRootPath + type + "/output/" + layerName + fileExt), ExportType.SAVEFORWEB, exportOptions);
+    // Hide the layer
+    docRef.layers[i].visible = false;
+  }
+
+  // Close the document
+  docRef.close(SaveOptions.DONOTSAVECHANGES);
+}
